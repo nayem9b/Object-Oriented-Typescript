4945d5018b83d8a9eaf910ab75101df3a79168ba
nayem9b <razwanniam1@gmail.com>
2023-05-05 11:29:19 +0600

    using public

--- src/object.ts
+++ src/object.ts
@@ -1,10 +1,9 @@
 class Animal {
-  name: string;
-  species: string;
-  sound: string;
-  constructor(name: string, species: string, sound: string) {
-    (this.name = name), (this.species = species), (this.sound = sound);
-  }
+  constructor(
+    public name: string,
+    public species: string,
+    public sound: string
+  ) {}
 
   makeSound() {
     console.log(`this ${this.name} says ${this.sound}`);
