bdfba08409282d851d085218e4e2676e93733a72
nayem9b <razwanniam1@gmail.com>
2023-05-06 08:38:40 +0600

    instance of guard

--- src/TypeGuard.ts
+++ src/TypeGuard.ts
@@ -22,6 +22,7 @@ type AdminUser = {
 
 function getUser(user: NormalUser | AdminUser) {
   if ("role" in user) {
+    //!Key in here
     return "I'm an Admin User";
   } else {
     return "I'm a normal user";
@@ -32,3 +33,47 @@ const normalUser1: NormalUser = { name: "Mr Reshob" };
 const adminUser1: AdminUser = { name: "Mr Reshob", role: "admin" };
 console.log(getUser(normalUser1));
 console.log(getUser(adminUser1));
+
+//! Instance of Guard
+class Animals {
+  name: string;
+  species: string;
+  constructor(name: string, species: string) {
+    this.name = name;
+    this.species = species;
+  }
+  makeSound() {
+    console.log("I'm making sound");
+  }
+}
+class Dog extends Animals {
+  constructor(name: string, species: string) {
+    super(name, species);
+  }
+  makeBark() {
+    console.log("I'm barking");
+  }
+}
+class Cat extends Animals {
+  constructor(name: string, species: string) {
+    super(name, species);
+  }
+  makeCall() {
+    console.log("I'm meowing");
+  }
+}
+
+function getAnimal(obj: Animals) {
+  if (obj instanceof Dog) {
+    obj.makeBark();
+  } else if (obj instanceof Cat) {
+    obj.makeCall();
+  } else {
+    obj.makeSound();
+  }
+}
+
+const animal1 = new Dog("German Bhai", "dog"); //!Dog er instance
+const animal2 = new Cat("Persian Bhai", "cat");
+
+getAnimal(animal2);
